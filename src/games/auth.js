4424be9a9632251b64a3de33d1d83e602fe57978
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-08 08:58:23 +0300

    remove auth

--- src/games/auth.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-
-console.log('Welcome to the Brain Games!');
-console.log('');
-export const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name);
