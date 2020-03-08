68bf41c556765d0dc823a049ab3dc84bb43364f3
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-08 08:57:34 +0300

    move even-game

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,23 +1,22 @@
 import readlineSync from 'readline-sync';
-import {name} from './auth.js';
+import {name} from '../greeting.js'
 
 const evenOrNot = () => {
   console.log('Answer "yes" if the number is even, otherwhise answer "no".')
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = (Math.floor(Math.random()*100) + 1);
-    const isEven = randomNumber % 2 === 0;
-    const isNotEven = randomNumber  % 2 !== 0;
+    const result = randomNumber % 2 === 0 ? 'yes' : 'no';
     console.log('Question: ', randomNumber);
     const answer = readlineSync.question('Your answer: ');
-    if ((isEven && answer === 'yes') || (isNotEven && answer === 'no')) {
+
+    if (result === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${result}"`);
       break;
     }
   }
   console.log(`Congratulations!, ${name}`)
 };
-evenOrNot();
 
 export default evenOrNot;
