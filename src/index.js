dd6a8651fe79a007e8d1baa0f22e26687ea1cdd6
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-08 08:58:42 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-export {name} from './games/auth.js';
-export {evenOrNot} from './games/even_game.js';
+// import readlineSync from 'readline-sync';
+// import * as greeting from './greeting.js';
+// import evenOrNot from './games/even_game.js'
+//
+// const game = evenOrNot();
+
+
+// import readlineSync from 'readline-sync';
+// import * as greeting from './greeting.js';
+// import calc from './games/calc-game.js'
+//
+// const game = calc();
+
+import readlineSync from 'readline-sync';
+import * as greeting from './greeting.js';
+import gcd from './games/gcd-game.js'
+
+const game = gcd();
