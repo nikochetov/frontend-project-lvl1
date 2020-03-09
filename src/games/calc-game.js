4377b92736db6a6c7fe88298eb4b7dad1aeb62b4
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-09 23:40:40 +0300

    update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,42 +1,28 @@
-import readlineSync from 'readline-sync';
-import {name} from '../greeting.js'
+import workFlow from '../index.js';
 
-const calc = () => {
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const firstOperand = (Math.floor(Math.random()*100 / 2) + 1);
-    const secondOperand = (Math.floor(Math.random()*10) + 1);
-
-    const randomOperator = () => {
-      let operator = "";
-      const possible = "+-*";
-      for(let i = 0; i < 1; i += 1) {
-        operator += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
-    console.log('Question: ', `${firstOperand} ${operator} ${secondOperand}`);
-    return operator;
-    };
-
-    let result = 0;
-      switch (randomOperator()) {
-        case '*':
-          result = firstOperand * secondOperand;
-        break;
-        case '-':
-          result = firstOperand - secondOperand;
-        break;
-        default:
-          result = firstOperand + secondOperand;
-      }
-    const answer = Number(readlineSync.question('Your answer: '));
-    if (result === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${result}"`);
+const description = 'What is the result of the expression?';
+const randomCalculate = () => {
+  const firstOperand = (Math.floor(Math.random()*100 / 2) + 1);
+  const secondOperand = (Math.floor(Math.random()*10) + 1);
+  let operator = "";
+  const possible = "+-*";
+  for(let i = 0; i < 1; i += 1) {
+    operator += possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+  let result = 0;
+    switch (operator) {
+      case '*':
+        result = firstOperand * secondOperand;
       break;
-    }
-  }
-  console.log(`Congratulations!, ${name}`)
+      case '-':
+        result = firstOperand - secondOperand;
+      break;
+      default:
+        result = firstOperand + secondOperand;
+    };
+  const question = `${firstOperand} ${operator} ${secondOperand}`;
 };
 
-export default calc;
+workFlow(description, randomCalculate)
+
+export default () => workFlow(description, randomCalculate);
