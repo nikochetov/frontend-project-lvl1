9a4d414aacd9e3112a8430aef2c3145cd00bcaf4
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-09 23:41:57 +0300

    update progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,28 +1,19 @@
-import readlineSync from 'readline-sync';
-import {name} from '../greeting.js'
+import workFlow from '../index.js';
 
+const description = 'What number is missing in the progression?';
 const progression = () => {
-  console.log('What number is missing in the progression?');
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = (Math.floor(Math.random()*100 ) + 1);
-    const randomNumber2 = (Math.floor(Math.random()*10 ) + 1);
-    let arr = [];
-    for (let j = randomNumber; j <= randomNumber + 10; j += 1) {
-      arr.push(j);
-    }
-    const result = arr[randomNumber2];
-    arr[randomNumber2] = '..';
-    const nums = arr.join(' ');
-    console.log('Question: ', `${nums}`);
-    const answer = Number(readlineSync.question('Your answer: '));
-    if (result === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${result}"`);
-      break;
-    }
-  }
-  console.log(`Congratulations!, ${name}`)
+  const randomNumber = (Math.floor(Math.random()*100 ) + 1);
+  const randomNumber2 = (Math.floor(Math.random()*10 ) + 1);
+  let arr = [];
+  for (let i = randomNumber; i <= randomNumber + 10; i += 1) {
+    arr.push(i);
+  };
+  const result = arr[randomNumber2];
+  arr[randomNumber2] = '..';
+  const nums = arr.join(' ');
+  const question = `${nums}`;
 };
 
-export default progression;
+workFlow(description, progression)
+
+export default () => workFlow(description, progression);
