a8a98e47aca858f5022e97f6e7a54af715243781
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-09 23:42:52 +0300

    remove greeting.js

--- src/greeting.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-export const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name);
