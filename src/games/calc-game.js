48115cf2d6dce5aa8da56db48743576ccb1a56ad
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-16 13:49:14 +0300

    update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,30 +1,29 @@
 import gameEngine from '../index.js';
+import { generateRandomNumber } from '../commonFunc.js';
 
 const description = 'What is the result of the expression?';
 const randomCalculate = () => {
-  const firstOperand = (Math.floor(Math.random()*100 / 2) + 1);
-  const secondOperand = (Math.floor(Math.random()*10) + 1);
-  let operator = "";
-  const possible = "+-*";
-  for(let i = 0; i < 1; i += 1) {
-    operator += possible.charAt(Math.floor(Math.random() * possible.length));
-  };
-  let correctAnswer = 0;
-    switch (operator) {
-      case '*':
-        correctAnswer = firstOperand * secondOperand;
-      break;
-      case '-':
-        correctAnswer = firstOperand - secondOperand;
-      break;
-      default:
-        correctAnswer = firstOperand + secondOperand;
-    };
+  const firstOperandRange = 100,
+        secondOperandRange = 20,
+        firstOperand = generateRandomNumber(firstOperandRange),
+        secondOperand = generateRandomNumber(secondOperandRange);
+  const possibleOperator = '+-*';
+  const index = Math.floor(Math.random() * possibleOperator.length);
+  const operator = possibleOperator[index];
+  let correctAnswer;
+  if (operator === '*') {
+    correctAnswer = firstOperand * secondOperand;
+  }
+  if (operator === '-') {
+    correctAnswer = firstOperand - secondOperand;
+  }
+  if (operator === '+') {
+    correctAnswer = firstOperand + secondOperand;
+  }
   const question = `${firstOperand} ${operator} ${secondOperand}`;
   const gameData = [question, correctAnswer];
   return gameData;
 };
 
-gameEngine(description, randomCalculate)
 
 export default () => gameEngine(description, randomCalculate);
