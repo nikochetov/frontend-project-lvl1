a24d9ce0649d735de9fe87ba18acdd2936ced5e8
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-22 23:23:34 +0300

    update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,29 +1,28 @@
 import gameEngine from '../index.js';
-import { generateRandomNumber } from '../commonFunc.js';
+import generateRandomNumber from '../randomNumber.js';
 
 const description = 'What is the result of the expression?';
 const randomCalculate = () => {
-  const firstOperandRange = 100,
-        secondOperandRange = 20,
-        firstOperand = generateRandomNumber(firstOperandRange),
-        secondOperand = generateRandomNumber(secondOperandRange);
+  const firstOperand = generateRandomNumber(0, 100);
+  const secondOperand = generateRandomNumber(0, 20);
   const possibleOperator = '+-*';
   const index = Math.floor(Math.random() * possibleOperator.length);
   const operator = possibleOperator[index];
   let correctAnswer;
-  if (operator === '*') {
-    correctAnswer = firstOperand * secondOperand;
-  }
-  if (operator === '-') {
-    correctAnswer = firstOperand - secondOperand;
-  }
-  if (operator === '+') {
-    correctAnswer = firstOperand + secondOperand;
-  }
+  switch (operator) {
+    case '*':
+      correctAnswer = firstOperand * secondOperand;
+    break;
+    case '-':
+      correctAnswer = firstOperand - secondOperand;
+    break;
+    case '+':
+      correctAnswer = firstOperand + secondOperand;
+    break;
+  };
   const question = `${firstOperand} ${operator} ${secondOperand}`;
   const gameData = [question, correctAnswer];
   return gameData;
 };
-
 
 export default () => gameEngine(description, randomCalculate);
