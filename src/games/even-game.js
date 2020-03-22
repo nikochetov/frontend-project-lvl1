3b9c9fccf3bd15f507b39280f715ddae412011e2
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-22 23:23:59 +0300

    update even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,13 +1,12 @@
 import gameEngine from '../index.js';
-import { generateRandomNumber } from '../commonFunc.js';
+import generateRandomNumber from '../randomNumber.js';
 
-const description = 'Answer "yes" if the number is even, otherwhise answer "no".'
-const checkForEven = () => {
-  const randomNumberRange = 100,
-        randomNumber = generateRandomNumber(randomNumberRange),
-        correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no',
-        gameData = [randomNumber, correctAnswer];
+const description = 'Answer "yes" if the number is even, otherwhise answer "no".';
+const genEvenNum = () => {
+  const randomNumber = generateRandomNumber(0, 100);
+  const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+  const gameData = [randomNumber, correctAnswer];
   return gameData;
 };
 
-export default () => gameEngine(description, checkForEven);
+export default () => gameEngine(description, genEvenNum);
