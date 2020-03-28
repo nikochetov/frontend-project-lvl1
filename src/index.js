c90aff09e05ea1523caf67438979dff28d269d4e
Nikolay Kochetov <nikochetov@gmail.com>
2020-03-28 14:33:54 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,23 +7,20 @@ const gameEngine = (description, getGameData) => {
   const playerName = readlineSync.question('May I have your name? ');
   console.log('Hello, ' + playerName);
   console.log(description);
-  const attempts = 3;
-  for (let attemptCount = 1; attemptCount <= attempts; attemptCount += 1) {
+  const attemptQuantity = 3;
+  for (let attemptCount = 1; attemptCount <= attemptQuantity; attemptCount += 1) {
     const [question, correctAnswer] = getGameData();
     console.log('Question: ', question);
     const userResponse = readlineSync.question('Your answer: ');
     if (correctAnswer == userResponse) {
       console.log('Correct!');
-      if (attemptCount === attempts) {
-        console.log(`Congratulations!, ${playerName}`);
-      }
-
     } else {
       console.log(`"${userResponse}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-      console.log(`Let's try again, ${playerName}`);
-      break;
+      return console.log(`Let's try again, ${playerName}`);
     }
   }
+
+  return console.log(`Congratulations!, ${playerName}`);
 };
 
 export default gameEngine;
