c564c79e264c09ed795376488995903af294d68c
Nikolay Kochetov <nikochetov@gmail.com>
2020-04-02 10:35:27 +0300

    update gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,15 +1,26 @@
 import gameEngine from '../index.js';
 import generateRandomNumber from '../randomNumber.js';
-import gcd from '../gcd.js';
+
+const gcd = (m, n) => {
+  if (m === n) {
+    return m;
+  }
+
+  if (m > n) {
+    return gcd(m - n, n);
+  }
+
+  return gcd(m, n - m);
+};
 
 const description = 'Find the greatest common divisor of given numbers.';
-const findGcd = () => {
-  const firstNumber = generateRandomNumber(0, 100);
-  const secondNumber = generateRandomNumber(0, 100);
+const getDataForGcdGame = () => {
+  const firstNumber = generateRandomNumber(1, 100);
+  const secondNumber = generateRandomNumber(1, 100);
   const correctAnswer = gcd(firstNumber, secondNumber);
   const question = `${firstNumber} ${secondNumber}`;
   const gameData = [question, correctAnswer];
   return gameData;
 };
 
-export default () => gameEngine(description, findGcd);
+export default () => gameEngine(description, getDataForGcdGame);
