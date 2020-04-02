a5fe00de3e2218f5038cef67820d6589dfb40389
Nikolay Kochetov <nikochetov@gmail.com>
2020-04-02 10:35:43 +0300

    update prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,29 +1,29 @@
 import gameEngine from '../index.js';
 import generateRandomNumber from '../randomNumber.js';
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const checkForPrime = () => {
-  const question = generateRandomNumber(0, 100);
-  const isPrime = (num) => {
-    if (num <= 1) {
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+
+  let i = 2;
+  while (i <= num / 2) {
+    if (num % i === 0) {
       return false;
     }
 
-    let i = 2;
-    while (i <= num / 2) {
-      if (num % i === 0) {
-        return false;
-      }
+    i += 1;
+  }
 
-      i += 1;
-    }
+  return true;
+};
 
-    return true;
-  };
-
-  const correctAnswer = isPrime(randomNumber) === true ? 'yes' : 'no';
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const getDataForPrimeGame = () => {
+  const question = generateRandomNumber(1, 100);
+  const correctAnswer = isPrime(question) === true ? 'yes' : 'no';
   const gameData = [question, correctAnswer];
   return gameData;
 };
 
-export default () => gameEngine(description, checkForPrime);
+export default () => gameEngine(description, getDataForPrimeGame);
