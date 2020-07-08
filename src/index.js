d6c2bfeb45aee452e32d08f160cf2945a621c269
Nikolay Kochetov <nikochetov@gmail.com>
2020-07-08 22:18:52 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const gameEngine = (description, getGameData) => {
   const greeting = 'Welcome to the Brain Games!';
   console.log(greeting);
   const playerName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + playerName);
+  console.log(`Hello, ${playerName}`);
   console.log(description);
   const attemptsCount = 3;
   for (let i = 1; i <= attemptsCount; i += 1) {
     const [question, correctAnswer] = getGameData();
     console.log('Question: ', question);
     const userResponse = readlineSync.question('Your answer: ');
-    if (correctAnswer == userResponse) {
+    if (correctAnswer === userResponse) {
       console.log('Correct!');
     } else {
       console.log(`"${userResponse}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
